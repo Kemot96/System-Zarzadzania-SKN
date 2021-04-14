f59aa2e9932b2f435674273624b2052baab98be3
KemotMen <tomekczerwinski3@tlen.pl>
2021-04-15 00:30:31 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClubController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SectionController;
+use App\Http\Controllers\ClubMemberController;
+use App\Models\Club;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+Route::resource('users', UserController::class);
+Route::resource('clubs', ClubController::class);
+Route::resource('sections', SectionController::class);
+Route::resource('clubMembers', ClubMemberController::class);
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +32,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/index', [App\Http\Controllers\ClubController::class, 'frontPage'])->name('index');
+
+Route::get('/{club}', [App\Http\Controllers\ClubController::class, 'mainPage'])->middleware(['auth', 'club.access'])->name('clubMainPage');
+
+Route::post('/{club}', [App\Http\Controllers\FileController::class, 'store'])->middleware(['auth', 'club.access'])->name('clubMainPageFile.store');
+//
+Route::get('/{club}/members', [App\Http\Controllers\ClubMemberController::class, 'index2'])->middleware(['auth'])->name('clubMembers.index2');
+Route::patch('/{club}/members/{clubMember}', [App\Http\Controllers\ClubMemberController::class, 'update2'])->middleware(['auth'])->name('clubMembers.update2');
+Route::delete('/{club}/members/{clubMember}', [App\Http\Controllers\ClubMemberController::class, 'destroy2'])->middleware(['auth'])->name('clubMembers.destroy2');
+//
+Route::get('/{club}/join', [App\Http\Controllers\ClubMemberController::class, 'joinPage'])->name('joinClub');
+
+Route::get('/{club}/join/send', [App\Http\Controllers\ClubMemberController::class, 'join'])->name('join');
+
 require __DIR__.'/auth.php';
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
