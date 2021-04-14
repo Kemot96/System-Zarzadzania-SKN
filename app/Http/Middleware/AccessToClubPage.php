535297de4327347eb94a05a627ed13f3896dfb5a
KemotMen <tomekczerwinski3@tlen.pl>
2021-04-15 00:29:39 +0200

    added new middleware

---
+++ app/Http/Middleware/AccessToClubPage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AcademicYear;
+use App\Models\ClubMember;
+use App\Models\Role;
+use App\Models\Club;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AccessToClubPage
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $club = $request->club;
+        $role_name = $club->getLoggedUserRoleName();
+        if($role_name == 'opiekun_koła'|| $role_name == 'członek_koła' || $role_name == 'przewodniczący_koła')
+        {
+            return $next($request);
+        }
+        return redirect(route('joinClub', ['club' => $club]));
+    }
+}
