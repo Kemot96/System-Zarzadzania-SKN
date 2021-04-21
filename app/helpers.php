49d8b1d78ffec2b343766c25a1d52486f66561ae
KemotMen <tomekczerwinski3@tlen.pl>
2021-04-21 02:11:44 +0200

    added dompdf library and created helpers.php

---
+++ app/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+
+use App\Models\AcademicYear;
+
+if (! function_exists('getCurrentAcademicYear')) {
+    function getCurrentAcademicYear() {
+        return AcademicYear::latest()->where('current_year', '1')->first()->id;
+    }
+}
+
